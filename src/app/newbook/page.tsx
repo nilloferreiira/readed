a5ee03437d78c6c193130fa68a9d4a911b520943
nilloferreiira <danillodana@outlook.com>
2024-01-15 21:57:10 -0300

    newBook page AddButton and responsivity

--- src/app/newbook/page.tsx
+++ src/app/newbook/page.tsx
@@ -6,12 +6,19 @@ export default function FormBookPage() {
       <Header />
 
       {/* main content */}
-      <div className="w-4/5 flex flex-col items-center justify-center mx-auto">
+      <div className={`
+                      w-4/5 
+                      gap-48
+                      mx-auto
+                      flex flex-col 
+                      lg:flex-row
+                      items-center lg:items-start justify-between
+                    `}>
         {/* container 1 */}
-        <div className="flex flex-col items-center justify-center w-full gap-5">
+        <div className="flex flex-col items-center lg:items-start justify-center w-full lg:w-3/5 gap-5">
           <h1 className="h1">Novo livro</h1>
           {/* form */}
-          <form className="flex flex-col w-full gap-3">
+          <form className="flex flex-col w-full lg:w-3/5 gap-3">
             <input
               type="text"
               placeholder="Nome do livro"
@@ -43,7 +50,6 @@ export default function FormBookPage() {
                         bg-darkBlue rounded-lg 
                         placeholder-fontwhite placeholder-opacity-80 placeholder-style
                         outline-none
-                        resize-none
                         min-h-32
                         `}
             />
@@ -51,7 +57,27 @@ export default function FormBookPage() {
         </div>
 
         {/* container 2 */}
-        <div></div>
+        <div className="flex flex-col items-center justify-center lg:w-3/5 mt-8 lg:mt-16 gap-5">
+          {/* stars  */}
+          <div>
+            stars component
+          </div>
+
+          <button
+            className={`
+                bg-indigo 
+                shadow-lg shadow-blue-500/20
+              text-fontWhite
+                opacity-85
+                py-3 px-6 
+                rounded-lg 
+                min-w-32 
+                hover:opacity-100 
+                transition-all outline-none
+              `}>
+            Adicionar
+          </button>
+        </div>
       </div>
     </div>
   );
