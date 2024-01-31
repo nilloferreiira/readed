20e11ce8ab3e1727ca3a422a8f74a8f75c3404b4
nilloferreiira <danillodana@outlook.com>
2024-01-31 16:03:29 -0300

    correcao shadcn/ui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,11 @@ import { Logo } from "../logo";
 import User from "./User";
 
 export function Header() {
-    return (
-        <header className="flex justify-between items-center p-8">
-        <Logo />
-        <User />
-      </header>
-    )
+  return (
+    <header className="flex justify-between items-center p-8">
+      <Logo />
+      
+      <User />
+    </header>
+  );
 }
